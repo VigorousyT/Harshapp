f9575ae252c0413d381d479f52cdea961294b322
Harsh <Harshyadav082000@gmail.com>
2022-08-25 16:35:37 +0530

    JAVASCRIPT

--- KnowledgeHut/Khut2__Java/Working with Arrays.js
+++ KnowledgeHut/Khut2__Java/Working with Arrays.js
@@ -84,6 +84,61 @@ books.forEach((elem, index)=> console.log(`${index + 1}. ${elem.title}`));
 
 
 
-/* - Hands-on Exercise
-code example:-  */
+/* Finding an Element in an Array  - Hands-on Exercise
+Defination--------Out of the many ways of finding elements in Arrays, there are two that are quite intuitive and extremely useful. the find() method allows you to implement a search function that can test for a condition to validate and find the very first matching element from an Array. 
+On the other hand, the includes() method tests for the presence of an element in an Array and can be smartly used to test multiple conditions without implementing a complicated if/switch conditional logic.  
+code example:- */
+
+
+'use strict';
+const Books = [
+  {
+    id: 1,
+    title: 'The Design of Everyday Things',
+    author: 'Don Norman',
+    formats: ['Paperback', 'Hardbound', 'eBook'],
+  },
+  {
+    id: 2,
+    title: 'No Logo',
+    author: 'Naomi Klein',
+    formats: ['Paperback', 'eBook'],
+  },
+  {
+    id: 3,
+    title: 'The Da Vinci Code',
+    author: 'Dan Brown',
+    formats: ['Paperback', 'eBook'],
+  },
+  {
+    id: 4,
+    title: 'Gold',
+    author: 'Isaac Asimov',
+    formats: ['Hardbound', 'eBook'],
+  },
+];
+
+const isAvailable = (title, format, arr) => {
+  const getBook = arr.find((book) => book.title === title);
+  return getBook && getBook.formats.includes(format);
+};
+
+console.log(
+  `No Logo by Naomi Klein (Paperback): ${
+    isAvailable('No Logo', 'Paperback', books) ? 'Available' : 'Not Available'
+  }`
+);
+
+console.log(
+  `The Da Vinci Code by Dan Brown (Hardbound): ${
+    isAvailable('The Da Vinci Code', 'Hardbound', books)
+      ? 'Available'
+      : 'Not Available'
+  }`
+);
+
+
+
+/*   - Hands-on Exercise
+code example:- */
 
