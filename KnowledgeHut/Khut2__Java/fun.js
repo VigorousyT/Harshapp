4e542463909d08483311c490d7912c7de6de4326
VigorousyT <harshyadav082000@gmail.com>
2022-09-23 09:30:22 +0530

    Express Started

---
+++ KnowledgeHut/Khut2__Java/fun.js
@@ -0,0 +1,22 @@
+var a = [1, 2, 3]
+var b = a;
+a.length = 0
+
+console.log(a)
+console.log(b)
+
+
+
+
+
+// var obj = {};
+// var obj1 = new Object();
+var dog = {
+    name: "null",
+    breed: "pug"
+}
+
+var b = 'name';
+console.log(dog[b]);
+console.log(dog.name);
+// How the Arrays are passed in JavaScript?
