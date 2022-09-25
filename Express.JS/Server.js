a8488dfaeb4ff0475f15b5678609119db03e6fff
VigorousyT <harshyadav082000@gmail.com>
2022-09-25 10:09:51 +0530

    adding products in Express app.

--- Express.JS/Server.js
+++ Express.JS/Server.js
@@ -1,9 +1,15 @@
-const { sendFile, } = require("express")
 var express = require("express");
 var App = express();
+var Products = require('./Products')
+App.set('view element', 'pug');
+App.set('views', './views');
 
 App.get("/", function (req, res) {
     res.sendFile(__dirname + "/myPage.html")
+})
+
+App.get("/Products", function (req, res) {
+    res.render('ProductsHome.pug', { prod: Products });
 })
 
 App.listen(5000)
