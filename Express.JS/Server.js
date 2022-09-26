33dd9348eb2c6f3d1628ec9153c2f3caec53e9c8
VigorousyT <harshyadav082000@gmail.com>
2022-09-26 10:28:48 +0530

    initial commit

--- Express.JS/Server.js
+++ Express.JS/Server.js
@@ -12,4 +12,14 @@ App.get("/Products", function (req, res) {
     res.render('ProductsHome.pug', { prod: Products });
 })
 
+App.get("/pugpoducts/:id", function (req, res) {
+    var id = req.params.id;
+    var selectedProduct = Products.find((p) => {
+        if (p.id == id) {
+            return true;
+        }
+    })
+    res.render('pugproducts', { Products: selectedProduct })
+})
+
 App.listen(5000)
