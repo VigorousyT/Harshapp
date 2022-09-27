af58d244c4a4d3270c354e38c166b7dd916c23ae
VigorousyT <harshyadav082000@gmail.com>
2022-09-27 22:11:15 +0530

    installation

--- React-Express-App/react-express/src/App.js
+++ React-Express-App/react-express/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Quiz from './Quiz';
 
 function App() {
   return (
-    <div className="App">
-      <h1>WELCOME TO REACT-EXPRESS-COLAB</h1>
-      <h2>QUIZ APP...</h2>
+    <div>
+      <div className="App">
+        <h1>WELCOME TO 👌REACT-EXPRESS-COLAB .or. (KHICHDI🤣)</h1>
+        <h2>QUIZ APP...🤫</h2>
+      </div>
+      <div>
+        <Quiz></Quiz>
+      </div>
     </div>
   );
 }
